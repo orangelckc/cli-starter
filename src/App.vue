9b225ed405ce859ac8bec38e7c23888233fff2ee
orangelckc <276126767@qq.com>
2023-12-12 11:22:40 +0100

    chore: vue3-front模版

---
+++ src/App.vue
@@ -0,0 +1,16 @@
+<script lang="ts" setup>
+import { useAppStore } from '@/store'
+
+const { appName } = storeToRefs(useAppStore())
+</script>
+
+<template>
+  <div>
+    {{ appName }}
+    <RouterView />
+  </div>
+</template>
+
+<style lang="scss" scoped>
+
+</style>
---
+++ src/views/Home/index.vue
@@ -0,0 +1,13 @@
+<script lang="ts" setup>
+
+</script>
+
+<template>
+  <div class="text-red-300">
+    Home
+  </div>
+</template>
+
+<style lang="scss" scoped>
+
+</style>
